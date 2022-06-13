ae051e57b2ebb7c855ecabc22a350fabc873887a
lu cía santos ordóñez <analucia0189@gmail.com>
2022-06-13 01:40:25 -0600

    Updates

---
+++ precache-manifest.d2a710a2365cb439e3eb0eb2ffcbe65f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2dd06cba68bfe8c334d",
+    "url": "/rae-frontend-react/static/css/main.4abe3afd.chunk.css"
+  },
+  {
+    "revision": "c2dd06cba68bfe8c334d",
+    "url": "/rae-frontend-react/static/js/main.c2dd06cb.chunk.js"
+  },
+  {
+    "revision": "3654ee2900390357e677",
+    "url": "/rae-frontend-react/static/js/1.3654ee29.chunk.js"
+  },
+  {
+    "revision": "f464addc34efc63d13a7",
+    "url": "/rae-frontend-react/static/js/runtime~main.f464addc.js"
+  },
+  {
+    "revision": "fa0946ea32b2436e1b74a1c509622794",
+    "url": "/rae-frontend-react/static/media/copyicon.fa0946ea.svg"
+  },
+  {
+    "revision": "29912d615e25cbe88a5a73cf245d9990",
+    "url": "/rae-frontend-react/static/media/waitgif.29912d61.webp"
+  },
+  {
+    "revision": "561d2a4262dd2a4f189465363f1e31d5",
+    "url": "/rae-frontend-react/static/media/logo.561d2a42.png"
+  },
+  {
+    "revision": "62fff25d16e20d4f538f3c53e81d1657",
+    "url": "/rae-frontend-react/index.html"
+  }
+];
